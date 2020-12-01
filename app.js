270275edd9fe48ca86c10021b60365302519627e
Sylvie-B <unsolo@hotmail.fr>
2020-12-01 15:10:53 +0100

    avec un break

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 let para = document.getElementsByClassName("paragraphe");
 
 for(i = 0 ; i < para.length ; i ++){
+    if ( i >= para.length - 1 ){
+        break;
+    }
     let texte = para[i];
     texte.innerHTML = "paragraphe position " + (i+1);
 }
