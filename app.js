af87de6604d051f5c521bb6bc235793a8bb65e64
Sylvie-B <unsolo@hotmail.fr>
2020-12-02 09:59:10 +0100

    version2

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 let para = document.getElementsByClassName("paragraphe");
 
 for(i = 0 ; i < para.length ; i ++){
+
     if ( (++i % 2) === 0){
+        console.log("dans la boucle : " + i);
         continue;
     }
     else if ( i >= para.length - 1 ){
         break;
     }
-    let texte = para[i];
-    texte.innerHTML = "paragraphe position " + (i+1);
+
+    para[i].innerHTML = "paragraphe position " + (i+1);
+
 }
